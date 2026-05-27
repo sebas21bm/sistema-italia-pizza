2a8d15505f068e386919a6bc4bd7bd7f48bc5f81
YaraOrtizOlmos <yaraortizolmos@gmail.com>
2026-05-27 04:31:16 -0600

    Edicion de Vistas

--- src/main/java/mx/uv/sistemapizzeria/controladores/UsuarioClienteRegistroController.java
+++ src/main/java/mx/uv/sistemapizzeria/controladores/UsuarioClienteRegistroController.java
@@ -6,7 +6,11 @@ package mx.uv.sistemapizzeria.controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,12 +19,49 @@ import javafx.fxml.Initializable;
  */
 public class UsuarioClienteRegistroController implements Initializable {
 
+    @FXML
+    private TextField txt_nombres;
+    @FXML
+    private TextField txt_telefono;
+    @FXML
+    private TextField txt_correoElectronico;
+    @FXML
+    private TextField txt_calle;
+    @FXML
+    private TextField txt_numero;
+    @FXML
+    private TextField txt_codigoPostal;
+    @FXML
+    private TextField txt_ciudad;
+    @FXML
+    private Button btn_cancelar;
+    @FXML
+    private Button btn_guardar;
+    @FXML
+    private Button btn_cerrar;
+    @FXML
+    private TextField txt_apellidoPaterno;
+    @FXML
+    private TextField txt_apellidoMaterno;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        txt_nombres.setDisable(false);
     }    
+
+    @FXML
+    private void clicCancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicGuardar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicCerrar(ActionEvent event) {
+    }
     
 }
