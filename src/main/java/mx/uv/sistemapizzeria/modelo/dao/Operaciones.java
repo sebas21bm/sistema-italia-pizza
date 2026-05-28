42272116f710a9dada8e6bb0011a875f98a61e5e
Isaac <zS24013258@estudiantes.uv.mx>
2026-05-28 02:55:52 -0600

    Correccion menor de Interfaz de Operaciones

--- src/main/java/mx/uv/sistemapizzeria/modelo/dao/Operaciones.java
+++ src/main/java/mx/uv/sistemapizzeria/modelo/dao/Operaciones.java
@@ -1,11 +1,13 @@
 package mx.uv.sistemapizzeria.modelo.dao;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface Operaciones<T, E> {
     E buscar(T identificador) throws Exception;
     boolean editar(E elemento) throws Exception;
     boolean eliminar(T identificador) throws Exception;
-    List<E> mostrarTodos() throws Exception;
+    List<E> mostrarTodos() throws NullPointerException, ClassNotFoundException, IOException, SQLException;
     boolean registrar(E elemento) throws Exception;
 }
